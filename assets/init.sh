c87ecd3924bb4e8480ec5a491c9b4a435e51ff24
Martin Ansong <martin.ansong@harness.io>
2022-02-16 20:43:17 +0000

    update init file to run on amazon linux

--- assets/init.sh
+++ assets/init.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
+sudo yum update -y
+
+sudo yum install docker -y
+
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
+
 mkdir -p /runner
-sudo apt-get update -y
-
-sudo apt-get -y install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-
-sudo usermod -aG docker $USER
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
 ssh-keygen -f /runner/id_rsa -q -P ""
-cd /runner
-sudo docker-compose up -d
