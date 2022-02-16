506e521b7df93efc272b9d673092e3612151b314
Martin Ansong <martin.ansong@harness.io>
2022-02-16 20:43:29 +0000

    refactor stack

--- vmdelegate/vmdelegate_stack.py
+++ vmdelegate/vmdelegate_stack.py
@@ -1,25 +1,20 @@
+import os
+from configparser import ConfigParser
 from jinja2 import FileSystemLoader, Environment
 from aws_cdk import (
+    aws_ec2 as ec2,
+    aws_iam as iam,
     CfnOutput,
     Stack,
+    Duration
 )
-import os
-import os.path
-import aws_cdk.aws_ec2 as ec2
 from constructs import Construct
 
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_access_key_secret = os.getenv('AWS_SECRET_ACCESS_KEY')
+cfg = ConfigParser()
+cfg.read('config.ini')
 
-ec2_type = "t3.medium"
-key_name = "mansong"
-harness_delegate_name = "aws-cdk-delegate-harness"
-harness_org_identifier = "default"
-harness_project_identifier = ""
-harness_account_id = "BKB_Vic2RbWnsMSUAWTgXw"
-harness_account_secret = "fed4d8b84c91279e84e4f2acc87ba440"
-linux_instance_type = "t3.medium",
-windows_instance_type = "t3.medium",
+aws_config = cfg['AWSINFO']
+harness_config = cfg['HARNESSINFO']
 
 linux_ami_id = ec2.GenericLinuxImage({
     "us-east-2": "ami-0fb653ca2d3203ac1",
@@ -28,10 +23,6 @@ linux_ami_id = ec2.GenericLinuxImage({
 windows_ami_id = ec2.GenericWindowsImage({
     "us-east-2": "ami-04d852871ae97b000",
 })
-
-delegate_tags = "ubuntu,linux,pool,aws,windows,cdk"
-
-dirname = os.path.dirname(__file__)
 
 with open("assets/init.sh", 'r') as init_script:
     init_script_contents = init_script.read()
@@ -42,48 +33,48 @@ templates = Environment(
 )
 
 env_file = templates.get_template("env.j2").render(
-    aws_access_key_id=aws_access_key_id,
-    aws_access_key_secret=aws_access_key_secret,
+    key_name=aws_config["key_name"],
     aws_region=os.getenv('AWS_DEFAULT_REGION'),
-    key_name=key_name
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_access_key_secret=os.getenv('AWS_SECRET_ACCESS_KEY'),
 )
 
 docker_compose = templates.get_template("docker-compose.yml.j2").render(
-    harness_account_id=harness_account_id,
-    harness_account_secret=harness_account_secret,
-    harness_delegate_name=harness_delegate_name,
-    harness_org_identifier=harness_org_identifier,
-    harness_project_identifier=harness_project_identifier
+    delegate_tags=harness_config["harness_delegate_tags"],
+    harness_account_id=harness_config["harness_account_id"],
+    harness_account_secret=harness_config["harness_account_secret"],
+    harness_delegate_name=harness_config["harness_delegate_name"],
+    harness_org_identifier=harness_config["harness_org_identifier"],
+    harness_project_identifier=harness_config["harness_project_identifier"]
 )
-
 
 class VmdelegateStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # VPC
         vpc = ec2.Vpc(
-            self, "harness-vpc",
-            cidr="10.0.0.0/16",
-            max_azs=2,
+            self, "VPC",
             nat_gateways=0,
-            enable_dns_hostnames=True,
-            enable_dns_support=True,
-            subnet_configuration=[
-                ec2.SubnetConfiguration(
-                    name="public",
-                    subnet_type=ec2.SubnetType.PUBLIC,
-                    cidr_mask=24
-                ),
-                # ec2.SubnetConfiguration(
-                #     name="private",
-                #     subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT,
-                #     cidr_mask=24
-                # )
-            ]
+            subnet_configuration=[ec2.SubnetConfiguration(name="public", subnet_type=ec2.SubnetType.PUBLIC)]
         )
 
         subnet_public = vpc.public_subnets[0]
+
+        # AMI
+        amzn_linux = ec2.MachineImage.latest_amazon_linux(
+            generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            edition=ec2.AmazonLinuxEdition.STANDARD,
+            virtualization=ec2.AmazonLinuxVirt.HVM,
+            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE
+        )
+
+        # Instance Role and SSM Managed Policy
+        role = iam.Role(self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
+
+        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
+
         security_group = ec2.SecurityGroup(
             self, "SecurityGroup",
             vpc=vpc,
@@ -112,55 +103,39 @@ class VmdelegateStack(Stack):
                 vpc_id=vpc.vpc_id,
                 subnet_id=subnet_public.subnet_id,
                 security_group=security_group.security_group_id,
-                linux_instance_type=linux_instance_type,
                 linux_ami_id=linux_ami_id,
-                windows_instance_type=windows_instance_type,
                 windows_ami_id=windows_ami_id,
+                linux_pool_instance_type=aws_config["linux_pool_instance_type"],
+                windows_pool_instance_type=aws_config["windows_pool_instance_type"],
             )
 
-        host = ec2.Instance(
+        instance = ec2.Instance(
             self, "HarnessDelegate",
-            instance_type=ec2.InstanceType(instance_type_identifier=ec2_type),
+            instance_type=ec2.InstanceType(instance_type_identifier=harness_config["harness_delegate_instance_type"]),
             instance_name="HarnessDelegate",
-            machine_image=linux_ami_id,
+            machine_image=amzn_linux,
             vpc=vpc,
             security_group=security_group,
-            key_name=key_name,
+            key_name=aws_config['key_name'],
             user_data=ec2.UserData.custom(init_script_contents),
-            vpc_subnets=subnet_public,
             init=ec2.CloudFormationInit.from_config_sets(
                 config_sets={
-                    "default": ["aptPreInstall", "config"]
+                    "default": ["config"]
                 },
                 configs={
-                    "apt_preInstall": ec2.InitConfig([
-                        ec2.InitPackage.apt("docker.io")]),
                     "config": ec2.InitConfig([
-                        ec2.InitGroup.from_name("docker"),
-
                         ec2.InitFile.from_string("/runner/.env.yml", env_file, base64_encoded=True),
-                        ec2.InitFile.from_string("/runner/.drone_pool.yml",drone_pool, base64_encoded=True),
+                        ec2.InitFile.from_string("/runner/.drone_pool.yml", drone_pool, base64_encoded=True),
                         ec2.InitFile.from_string("/runner/docker-compose.yml", docker_compose, base64_encoded=True),
+                        ec2.InitGroup('docker'),
+                        ec2.InitCommand.shell_command("sudo docker-compose up -d")
                     ])
-                }
-                ),
+                }),
             init_options=ec2.ApplyCloudFormationInitOptions(
                 config_sets=["default"],
+                ignore_failures=False,
+                timeout=Duration.minutes(10)
             )
         )
 
-        host.instance.add_property_override("BlockDeviceMappings", [{
-            "DeviceName": "/dev/xvda",
-            "Ebs": {
-                "VolumeSize": "10",
-                "VolumeType": "io1",
-                "Iops": "150",
-                "DeleteOnTermination": "true"
-            }
-        }, {
-            "DeviceName": "/dev/sdb",
-            "Ebs": {"VolumeSize": "30"}
-        }
-        ])  # by default VolumeType is gp2, VolumeSize 8GB
-
-        CfnOutput(self, "Output", value=host.instance_public_ip)
+        CfnOutput(self, "Output", value=instance.instance_public_ip)
